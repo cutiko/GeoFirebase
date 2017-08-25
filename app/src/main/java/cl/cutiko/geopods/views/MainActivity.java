8f0065b10e2b4ccd67060dcf0fcd6049b599af7d
cutiko <cutiko@gmail.com>
2017-08-25 11:49:14 -0300

    ADD: ready to start

--- app/src/main/java/cl/cutiko/geopods/views/MainActivity.java
+++ app/src/main/java/cl/cutiko/geopods/views/MainActivity.java
@@ -6,18 +6,10 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.telephony.TelephonyManager;
 import android.view.View;
-
-import com.google.firebase.database.DatabaseReference;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import cl.cutiko.geopods.R;
 import cl.cutiko.geopods.adapters.SectionsPagerAdapter;
-import cl.cutiko.geopods.data.Nodes;
-import cl.cutiko.geopods.models.GeoPod;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,33 +31,6 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                double latitude = -33.434874;
-                double longitude = -70.603061;
-                String name = "this is a great test!";
-                TelephonyManager telephonyManager = (TelephonyManager) MainActivity.this.getSystemService(TELEPHONY_SERVICE);
-                String countryIso = telephonyManager.getSimCountryIso();
-                String key = new Nodes().locations(countryIso).push().getKey();
-
-                GeoPod reduced = new GeoPod();
-                reduced.setLatitude(latitude);
-                reduced.setLongitude(longitude);
-                reduced.setName(name);
-                reduced.setKey(key);
-
-                GeoPod geoPod = new GeoPod();
-                geoPod.setLatitude(latitude);
-                geoPod.setLongitude(longitude);
-                geoPod.setName(name);
-                geoPod.setCategory("category1");
-                geoPod.setKey(key);
-
-                DatabaseReference reference = new Nodes().getRoot();
-                Map<String, Object> map = new HashMap<>();
-                map.put("locations/"+countryIso+"/"+key, reduced);
-                map.put("places/"+countryIso+"/"+key, geoPod);
-
-                reference.updateChildren(map);
-
 
             }
         });
--- app/src/main/java/cl/cutiko/geopods/views/map/MapFragment.java
+++ app/src/main/java/cl/cutiko/geopods/views/map/MapFragment.java
@@ -1,41 +1,16 @@
 package cl.cutiko.geopods.views.map;
 
-import android.content.Context;
-import android.location.Location;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.util.List;
 
 import cl.cutiko.geopods.R;
-import cl.cutiko.geopods.models.GeoPod;
-import cl.cutiko.geopods.models.PodsCallback;
 
 
-public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, PodsCallback {
-
-    private GoogleApiClient googleApiClient;
-    private GoogleMap googleMap;
+public class MapFragment extends Fragment {
 
     public static MapFragment newInstance() {
         return new MapFragment();
@@ -50,77 +25,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleA
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapContainer);
-        if (mapFragment != null) {
-            fragmentTransaction.remove(mapFragment);
-        }
-        fragmentTransaction.disallowAddToBackStack();
-        fragmentTransaction.add(R.id.mapContainer, SupportMapFragment.newInstance());
-        fragmentTransaction.commitNowAllowingStateLoss();
 
-        SupportMapFragment googleMap = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapContainer);
-        googleMap.getMapAsync(this);
-
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        this.googleMap = googleMap;
-
-        googleApiClient = new GoogleApiClient.Builder(getContext())
-                .addApi(LocationServices.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-        googleApiClient.connect();
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        Location currentLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-        LatLng current = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        //LatLng current = new LatLng(-33.429072, -70.603748);
-        TelephonyManager telephonyManager = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
-        String countryIso = telephonyManager.getSimCountryIso();
-        new PopulatePods(this).getNear(current, countryIso);
-        googleMap.addMarker(new MarkerOptions().position(current).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 14));
-
-        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                GeoPod geoPod = (GeoPod) marker.getTag();
-                if (geoPod != null) {
-                    Toast.makeText(getContext(), geoPod.getName(), Toast.LENGTH_SHORT).show();
-                }
-                return true;
-            }
-        });
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Toast.makeText(getContext(), "Conexión Suspendida", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Toast.makeText(getContext(), "Conexión Fallida", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        googleApiClient.disconnect();
-    }
-
-    @Override
-    public void podsReady(List<GeoPod> geoPods) {
-        for (GeoPod geoPod : geoPods) {
-            LatLng latLng = new LatLng(geoPod.getLatitude(), geoPod.getLongitude());
-            Marker marker = googleMap.addMarker(new MarkerOptions().position(latLng));
-            marker.setTag(geoPod);
-        }
     }
 }

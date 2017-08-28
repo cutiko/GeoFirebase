94003be4dfb111d461c4b455cb217e4ac46cbe7c
cutiko <cutiko@gmail.com>
2017-08-27 21:01:22 -0300

    FIX: model was not DRY

--- app/src/main/java/cl/cutiko/geopods/models/GeoPlace.java
+++ app/src/main/java/cl/cutiko/geopods/models/GeoPlace.java
@@ -1,31 +1,16 @@
 package cl.cutiko.geopods.models;
 
+import cl.cutiko.geofirebase.models.GeoPod;
+
 /**
  * Created by cutiko on 23-08-17.
  */
 
-public class GeoPlace extends cl.cutiko.geofirebase.models.GeoPod {
+public class GeoPlace extends GeoPod {
 
-    private double latitude, longitude;
-    private String name, category, key;
+    private String name, category;
 
     public GeoPlace() {
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
     }
 
     public String getName() {
@@ -42,14 +27,6 @@ public class GeoPlace extends cl.cutiko.geofirebase.models.GeoPod {
 
     public void setCategory(String category) {
         this.category = category;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
     }
 
 }
